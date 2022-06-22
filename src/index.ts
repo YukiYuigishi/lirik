d7776552dfc619072abcfa44e0e1b6c6aaa3303e
Yuki Yuigishi <me@yuigishi.dev>
2022-06-22 16:22:23 +0900

    feat: redirect

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import { Hono } from "hono";
+import {Hono} from "hono";
+import {serveStatic} from "hono/serve-static"
 import SearchRedirectUrl from "./redirect";
 
 const App = new Hono();
-
+App.use("*", serveStatic({root: "./"}));
 App.get("/", (c) => {
-  const { id } = c.req.query();
-  // const url = SearchRedirectUrl(id).catch(() => c.status(418));
-  const url = SearchRedirectUrl(id);
-  if (url !== undefined) {
-    return c.redirect(url, 307);
-  }
-  return c.text("I am a teapot.", 418);
+   const {id} = c.req.query();
+   const url = SearchRedirectUrl(id);
+   if (url !== undefined) {
+      return c.redirect(url, 307);
+   }
+   //   return c.html("<img src='/static/teapot.jpg' width='300' height='300'alt='I am a teapot.'>", 418);
+   return c.text("I am a teapot.", 418);
 });
 
 App.fire();
