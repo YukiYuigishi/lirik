8f767644a5b13313bfd9467c4eed1afb08fe501d
Yuki Yuigishi <me@yuigishi.dev>
2022-07-18 18:37:36 +0900

    feat/teapot が表示されるように変更

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import {Hono} from "hono";
-import {serveStatic} from "hono/serve-static"
+import { Hono } from "hono";
+import { serveStatic } from "hono/serve-static";
 import SearchRedirectUrl from "./redirect";
 
 const App = new Hono();
-App.use("*", serveStatic({root: "./"}));
+App.use("/static/*", serveStatic({ root: "./" }));
 App.get("/", (c) => {
-   const {id} = c.req.query();
-   const url = SearchRedirectUrl(id);
-   if (url !== undefined) {
-      return c.redirect(url, 307);
-   }
-   //   return c.html("<img src='/static/teapot.jpg' width='300' height='300'alt='I am a teapot.'>", 418);
-   return c.text("I am a teapot.", 418);
+  const { id } = c.req.query();
+  const url = SearchRedirectUrl(id);
+  if (url !== undefined) {
+    return c.redirect(url, 307);
+  }
+
+  return c.html(
+    "<img src='/static/teapot.jpg' alt='I am a teapot.'>",
+    418
+  );
+  //   return c.text("I am a teapot.", 418);
 });
 
 App.fire();
